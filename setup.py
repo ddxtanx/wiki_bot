1648acc8ab5aec3e24d77362b229a05a040bc8c4
Garrett Credi [ddxtanx] <gcc@ameritech.net>
2018-05-06 10:30:55 -0500

    Fix PyPI errors.

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
   name='wikiBot',
-  packages=['wikiBot'],
-  version='0.1',
+  version='0.3',
   description='A random wikipedia page generator.',
+  long_description='A bot that uses the Wikipedia API to enumerate ' +
+  'subcategories and subpages to generate a random page that is a member of ' +
+  'a given supercategory.',
   author='Garrett Credi',
   author_email='gcc@ameritech.net',
   url='https://github.com/ddxtanx/wikiBot',
-  download_url='https://github.com/ddxtanx/wikiBot/archive/0.1.tar.gz',
+  license='MIT',
+  download_url='https://github.com/ddxtanx/wikiBot/archive/0.2.tar.gz',
+  py_modules=["wikiBot"],
   keywords=['wikipedia', 'random', 'generator', 'command_line', 'api'],
   classifiers=[],
   install_requires=['requests']

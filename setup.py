8356afbb4e0352694af6ccedb8b3b184d4663c5d
Garrett Credi [ddxtanx] <gcc@ameritech.net>
2018-05-06 09:20:53 -0500

    Add code to PyPI.

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+  name='wikiBot',
+  packages=['wikiBot'],
+  version='0.1',
+  description='A random wikipedia page generator.',
+  author='Garrett Credi',
+  author_email='gcc@ameritech.net',
+  url='https://github.com/ddxtanx/wikiBot',
+  download_url='https://github.com/ddxtanx/wikiBot/archive/0.1.tar.gz',
+  keywords=['wikipedia', 'random', 'generator', 'command_line', 'api'],
+  classifiers=[],
+  install_requires=['requests']
+)
